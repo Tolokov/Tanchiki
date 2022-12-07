ca0d3308976889870ba2f041c197299fcf0d0d53
Python Dev <vitaliitolokov@gmail.com>
2022-12-07 23:00:36 +0500

    close #9 [x]-Инициализировать окно [x]-Заголовок [x]-Иконка [-]-Убрать рамку окна

--- Game(python_version)/main.py
+++ Game(python_version)/main.py
@@ -1,27 +1,43 @@
 import pygame
 
 
-def main():
-    # display size and background color
-    screen = pygame.display.set_mode((1000, 1000))
-    background_colour = (105, 105, 105)
-    screen.fill(background_colour)
+class GameWindow:
+    def __init__(self):
+        self.path_to_logo = '..\images\icon.png'
+        self.display_size = (1000, 1000)
+        self.background_color = (105, 105, 105)
+        self.title = 'Tanchiki'
+        self.status_run = True
 
-    # Title and FLIP
-    pygame.display.set_caption('Tanchiki')
-    pygame.display.flip()
+    def set_icon(self):
+        icon_img = pygame.image.load(self.path_to_logo)
+        pygame.display.set_icon(icon_img)
 
-    run = True
-    while run:
+    def create_window(self):
+        # display size and background color
+        screen = pygame.display.set_mode(self.display_size)
+        screen.fill(self.background_color)
 
-        for event in pygame.event.get():
+        # Title and FLIP
+        pygame.display.set_caption(self.title)
+        pygame.display.flip()
 
-            # Quit buttons
-            if event.type == pygame.QUIT:
-                run = False
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAP:
-                run = False
+    def build(self):
+        while self.status_run:
+            for event in pygame.event.get():
+
+                # Quit buttons
+                if event.type == pygame.QUIT:
+                    self.status_run = False
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAP:
+                    self.status_run = False
+
+    def run(self):
+        self.set_icon()
+        self.create_window()
+        self.build()
 
 
 if "__main__" == __name__:
-    main()
+    game = GameWindow()
+    game.run()
